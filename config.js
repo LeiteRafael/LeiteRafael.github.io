ecb29c6c46629b43437be4637a0ff6f430e3e907
Rafael Leite <rafael.leite@sensedia.com>
2021-02-08 17:31:22 -0300

    refatoracao

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteTitle: 'Rafael Leite', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Rafael ',
+  lastName: 'Leite',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/LeiteRafael',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/rafanleite',
+    },
+  ],
+  email: 'rafanleite@gmail.com',
+  address: 'Porto Alegre, Brasil',
+};
